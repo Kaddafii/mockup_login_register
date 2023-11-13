a9e887dce6116608b25a50cd95c97eaf7fbd5db5
Kaddafii <137488220+Kaddafii@users.noreply.github.com>
2023-11-13 13:35:28 +0700

    Add files via upload

---
+++ registPemilik.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+import 'loginPenyewa.dart';
+
+class registPemilik extends StatelessWidget {
+  const registPemilik({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Image.asset('assets/images/arrow.png'),
+          ),
+          title: Text(
+            'Registrasi pemilik',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              fontSize: 24,
+              color: Color.fromRGBO(29, 77, 79, 1.0),
+            ),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Align(
+                child: Image.asset('assets/images/Klogo3.png'),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Alamat Email
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0, vertical: 0.0),
+                  child: Text(
+                    "Alamat email",
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 15,
+                      color: Color.fromRGBO(29, 77, 79, 1),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "user@gmail.com",
+                          hintStyle: TextStyle(color: (Colors.grey[300])!),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // No. Handphone
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0, vertical: 0.0),
+                  child: Text(
+                    "No. Handphone",
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 15,
+                      color: Color.fromRGBO(29, 77, 79, 1),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "085XXXXXX",
+                          hintStyle: TextStyle(color: (Colors.grey[300])!),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Alamat
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0, vertical: 0.0),
+                  child: Text(
+                    "Alamat",
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 15,
+                      color: Color.fromRGBO(29, 77, 79, 1),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "alamat",
+                          hintStyle: TextStyle(color: (Colors.grey[300])!),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Password
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0, vertical: 0.0),
+                  child: Text(
+                    "Password",
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 15,
+                      color: Color.fromRGBO(29, 77, 79, 1),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      suffixIcon: const Icon(
+                        Icons.remove_red_eye,
+                        color: Colors.grey,
+                      ),
+                      hintText: "password",
+                      hintStyle: TextStyle(color: (Colors.grey[300])!),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: SizedBox(
+                      width: 242,
+                      height: 42,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromRGBO(40, 48, 72, 1),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          "Daftar",
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(0.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Belum Punya akun
+                        Text(
+                          "Sudah punya akun?",
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 12,
+                            color: (Colors.grey[500])!,
+                          ),
+                        ),
+                        // Daftar Button
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: ((context) => const loginPenyewa())),
+                            );
+                          },
+                          style: TextButton.styleFrom(
+                            foregroundColor: (Colors.grey[500])!,
+                          ),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 12,
+                              color: Color.fromRGBO(60, 139, 23, 1),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
